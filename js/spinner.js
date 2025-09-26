f701d9689db7674777b83420187d077e49f6a918
Mike <136083761+micheal4067@users.noreply.github.com>
2025-09-26 12:12:23 +0100

    Add files via upload

---
+++ js/spinner.js
@@ -0,0 +1,21 @@
+ document.addEventListener('DOMContentLoaded', () => {
+   
+    const loader = document.getElementById('global-loader');
+    if (loader) {
+      loader.innerHTML = `
+        <div class="spinner-wrapper">
+          <div class="spinner"></div>
+          <div class="spinner-icon-bg">
+            <img src="./images/title-icon.png" alt="Loading Icon" class="spinner-icon">
+          </div>
+        </div>`;
+    }
+  });
+
+  // Hide loader after page is fully loaded
+  window.addEventListener('load', () => {
+    const loader = document.getElementById('global-loader');
+    if (loader) {
+      loader.classList.add('hidden');
+    }
+  });
